66fd1ed989c94eb4083e4032b75954fabff7cda0
Maheshsal1104 <157306500+Maheshsal1104@users.noreply.github.com>
2025-09-22 15:26:06 +0530

    Update HelloJenkins.java

--- HelloJenkins.java
+++ HelloJenkins.java
@@ -1,5 +1,5 @@
 public class HelloJenkins {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub and Jenkins from Java!");
+        System.out.println("Hello again from GitHub and Jenkins! This is a code update.");
     }
 }
